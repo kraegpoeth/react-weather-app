f0dee460d2b80c4940b9c7c3933829e517422f0c
Kaserp Kraegpoeth <kraegpoeth@gmail.com>
2017-02-10 13:49:10 +0100

    prepped repo for heroku

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 var express = require('express');
 
 var app = express();
+const port = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
-
-app.listen(3000, function() {
-  console.log("server up in port 3000!");
-
-})
+app.listen(port, function() {
+  console.log("server up in port " + port);
+});
